430b3c3031e60bd3c9897880f1ff4037dfe8f33e
Sandippativala <155566645+Sandippativala@users.noreply.github.com>
2024-08-21 14:22:12 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,8 @@
+Total_card=52
+Kings=4
+Spades= 13
+neither_king_nor_spade=Total_card-Kings-Spades+1
+probability=neither_king_nor_spade/Total_card
+print("------------probability------------")
+print("The Probability of drawing a card that is neither a king nor a spade is :",probability)
+print("-----------------------------------")
