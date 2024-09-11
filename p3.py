133d2cbb9b94e594e84a5d1775e6c9a2507d3d0e
Sandippativala <155566645+Sandippativala@users.noreply.github.com>
2024-09-11 16:42:10 +0530

    Add files via upload

--- p3.py
+++ p3.py
@@ -1,26 +1,8 @@
-# p3 : Extracting field of a roll number using string  indexing and slicing.
+def a(list):
+    if(list!=[]):
+        print(list[0],end=" ")
+        print(list)
+        a(list[1:])
 
-roll_number="MCA462024"
-rollnumber='m','c','a','4','6','2','0','2','4'
-
-# slicing on roll number.
-RollNumber=roll_number[3:5]
-DepartmentName=roll_number[0:3]
-JoinYear=roll_number[5:]
-
-# indexing on roll nmber.
-DepartmentName1=rollnumber.index('m')
-JoinYear1=rollnumber.index('2')
-
-print("-----------------input string-----------------------")
-print("First :",roll_number)
-print("Second :",rollnumber)
-
-print("-----------------slicing find-----------------------")
-print("Roll Number :",RollNumber)
-print("Department Name :",DepartmentName)
-print("Joinnig Year Of Collage :",JoinYear)
-
-print("-----------------index find-----------------------")
-print("Department Name :",DepartmentName1)
-print("Joinnig Year Of Collage :",JoinYear1)
+list=[1,2,3,4,5,6,7,8,9,10]
+a(list)
